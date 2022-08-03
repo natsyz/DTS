943ea4bb264118de339d8140b92851ed8d06bc90
Natasya Zahra <natasya.zahra91@ui.ac.id>
2022-08-03 20:05:42 +0700

    Second draft

--- page.py
+++ page.py
@@ -44,7 +44,7 @@ st.markdown("""
     
     Sesuai namanya, email phising menggunakan media email untuk menjangkau calon korbannya.
     
-    ![alt text](image3.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image4.png?raw=true" alt="drawing" width="700"/>
 
     Jumlah aksi email phising ini cukup banyak. Menurut data, terdapat 3,4 miliar email palsu yang dikirimkan setiap harinya. Anda bisa bayangkan, berapa banyak korban yang bisa terjerat aksi ini.
 
@@ -63,7 +63,7 @@ st.markdown("""
     Web phising adalah upaya memanfaatkan website palsu untuk mengelabui calon korban. Website untuk phising akan terlihat mirip dengan website resmi dan menggunakan nama domain yang mirip. Hal ini disebut domain spoofing.
     
     Sebagai contoh, untuk menyerupai niagahoster.co.id, domain yang digunakan pelaku phising adalah niaga-hoster.my.id.
-""")
+""", unsafe_allow_html=True)
 
 
 st.markdown("## Bagaimana Sebuah Aksi Phising Dijalankan?")
@@ -75,7 +75,7 @@ st.markdown("""
 
     Tahap awal kegiatan web phising akan dimulai dengan menentukan siapa calon korbannya. Pada umumnya, korban yang disukai adalah pengguna platform pembayaran online seperti PayPal, Ovo, dan lainnya.
 
-    ![alt text](images/image4.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image5.png?raw=true" alt="drawing" width="700"/>
 
     Tidak hanya itu saja, banyak pelaku phising yang mengincar pengguna platform yang memiliki celah keamanan. Kasus terbaru terjadi pada platform komunikasi Zoom. Tak kurang dari 1000 upaya phising terjadi hanya di bulan April 2020 saja.
 
@@ -91,25 +91,25 @@ st.markdown("""
 
     Untuk melancarkan aksinya, pelaku akan mulai menyiapkan website palsu untuk melakukan aksi phising. Mulai dari mendesain website palsu, memilih nama domain yang mirip dengan domain asli hingga menyiapkan konten dengan tulisan yang meyakinkan.
 
-    ![alt text](images/image5.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image6.png?raw=true" alt="drawing" width="700"/>
 
     Pada prakteknya, pelaku kadang membuat website yang sangat menyerupai halaman website resmi tapi menggunakan nama domain yang jauh berbeda seperti terlihat di contoh atas.
 
     Namun, pada contoh kasus phishing Danamon Online beberapa waktu lalu, Anda akan langsung melihat bahwa domain yang digunakan mirip sekali dengan website resminya:
 
-    ![alt text](images/image6.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image7.png?raw=true" alt="drawing" width="700"/>
 
 4. **Calon Korban Mengakses Website Phising**
 
     Dengan tampilan website dan informasi yang meyakinkan, tak sedikit calon korban yang akhirnya mengakses website phising milik pelaku. Langkah ini biasanya didahului dengan mengajak calon korban melalui email phising atau link yang disebarkan via SMS atau akun media sosial.
 
-    ![alt text](images/image7.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image8.png?raw=true" alt="drawing" width="700"/>
 
 5. **Calon Korban Mengikuti Instruksi Pelaku**
 
     Inilah kunci dari terjadinya aksi phising. JIka calon korban melakukan instruksi yang diberikan pelaku, maka pelaku akan berhasil mencapai tujuannya.
 
-    ![alt text](images/image8.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image9.png?raw=true" alt="drawing" width="700"/>
 
     Sebagai contoh, pada halaman website yang disediakan, calon korban diminta melakukan update informasi pribadi hingga data pembayaran pada akun yang digunakan. Pada saat selesai mengisi data dan melakukan submit, saat itulah semua informasi korban berhasil dimiliki.
 
@@ -122,7 +122,7 @@ st.markdown("""
     * Menjalankan aksi penipuan. Misalnya, dengan menyatakan seseorang memenangkan undian tertentu yang pada akhirnya meminta orang tersebut mengirimkan sejumlah uang.
     * Menggunakan data yang dimiliki untuk mencoba membobol akun yang dimiliki atau akun lain.
     * Melakukan pinjaman online mengatasnamakan korban dengan menggunakan data diri lengkap korban. Tentu saja, korban lah yang akan ditagih pelunasan atas pinjaman tersebut.
-""")
+""", unsafe_allow_html=True)
 
 
 st.markdown("## 9 Tips Agar Tidak Menjadi Korban Phising")
@@ -134,7 +134,7 @@ st.markdown("""
 
     Anda sudah belajar tentang jenis phising. Namun, tidak menutup kemungkinan jenis kejahatan online akan terus berkembang. Baik dari media yang digunakan untuk phising ataupun jenis serangan yang dilakukan.
 
-    ![alt text](images/image9.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image10.png?raw=true" alt="drawing" width="700"/>
 
     Oleh karena itu, selalu ikuti berita perkembangan phising dengan baik. Salah satunya dengan memiliki rasa ingin tahu apabila ada insiden keamanan yang terjadi seperti kebocoran data pengguna Tokopedia atau Bukalapak baru-baru ini.
 
@@ -150,7 +150,7 @@ st.markdown("""
 
     Sebagai contoh, ketika Anda menerima email yang seolah berasal dari Niagahoster dan meminta Anda untuk melakukan pembaruan pesanan seperti di bawah ini, Anda bisa mengabaikannya:
 
-    ![alt text](images/image10.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image11.png?raw=true" alt="drawing" width="700"/>
 
     Apa alasannya?
 
@@ -158,19 +158,19 @@ st.markdown("""
 
     Kedua, pada bagian footer email, akan selalu ada kontak resmi Niagahoster yang bisa Anda gunakan melakukan pengecekan (verifikasi) apakah informasi yang Anda dapatkan itu benar. Pada email phising di atas, tidak ada, bukan?
 
-    ![alt text](images/image11.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image13.png?raw=true" alt="drawing" width="700"/>
 
 3. **Jangan Asal Klik Link yang Diterima**
 
     Meskipun Anda menjadi sasaran phising, Anda belum tentu menjadi korban. Kuncinya adalah apakah Anda melakukan klik pada link yang disiapkan oleh pelaku phising atau tidak.
 
-    ![alt text](images/image12.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image14.png?raw=true" alt="drawing" width="700"/>
 
     Seperti Anda tahu, email dan website untuk phising dibuat mirip dengan aslinya. Namun, selalu ada hal yang membedakan sumber resmi dengan palsu. Bisa dari form pengisian data yang mencurigakan, bahasa konten yang bukan seperti biasa Anda terima, dan lain sebagainya.
 
     Jadi, sebelum meng-klik link apapun, pastikan link tersebut aman. 
 
-    ![alt text](images/image13.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image15.png?raw=true" alt="drawing" width="700"/>
 
     Caranya, arahkan mouse ke link tersebut tanpa diklik (hover). Kemudian, akan muncul informasi URL dari link tersebut. Jika mengarah ke website asli, berarti aman. Jika mengarah ke website lain yang tidak dikenal, lebih baik urungkan niat Anda.
 
@@ -178,7 +178,7 @@ st.markdown("""
 
     Jangan kunjungi website yang tidak aman, terutama website yang akan memproses data pribadi atau finansial. Hanya lakukan transaksi pada website yang menggunakan SSL saja, yaitu website yang ditandai dengan penggunaan protokol HTTPS.
 
-    ![alt text](image14.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image16.png?raw=true" alt="drawing" width="700"/>
 
     Dengan memastikan aktivitas online Anda hanya pada website yang aman, maka kemungkinan Anda menjadi korban phising lebih kecil.
 
@@ -186,7 +186,7 @@ st.markdown("""
 
     Sarana Anda untuk melakukan aktivitas online adalah browser. Jadi, selalu gunakan versi browser terbaru yang dapat melindungi keamanan data dan privasi Anda.
 
-    ![alt text](images/image15.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image17.png?raw=true" alt="drawing" width="700"/>
 
     Langkah ini penting karena setiap browser merilis versi terbaru, selalu terkait dengan perbaikan pada celah keamanan dan fitur yang lebih efektif. Untuk memudahkan, Anda cukup mengaktifkan status automatic update di tiap browser yang Anda gunakan.
 
@@ -206,7 +206,7 @@ st.markdown("""
 
     Jika platform yang Anda gunakan menyediakannya, selalu aktifkan Two-Factor Authentication (2FA). Sistem ini menggunakan verifikasi 2 langkah, yaitu password dan ponsel Anda.
 
-    ![alt text](images/image16.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image18.png?raw=true" alt="drawing" width="700"/>
 
     Pada saat pelaku phising sudah menemukan username dan password Anda tapi tidak dapat memasukan kode verifikasi 2FA, platform tidak akan melanjutkan proses. Artinya, akun Anda akan terlindungi dengan lebih baik.
 
@@ -215,7 +215,7 @@ st.markdown("""
     Salah satu serangan dalam phising adalah meminta Anda mendownload file tertentu melalui email palsu yang Anda terima. Pada saat melakukannya, bisa saja Anda sedang mengunduh malware yang akan bekerja di komputer Anda secara rahasia.
 
     Untuk menghindari hal ini, gunakanlah software anti-malware yang akan melakukan scan secara otomatis sesuai dengan setting yang Anda gunakan. Jangan lupa untuk segera menghapus script yang mencurigakan yang bisa saja mencuri informasi pribadi Anda.
-""")
+""", unsafe_allow_html=True)
 
 st.markdown("## 4 Cara Mengatasi Phising di Website WordPress")
 
@@ -226,7 +226,7 @@ st.markdown("""
 
     Jangan sampai website Anda dimanfaatkan untuk sarana pencurian data pengunjung atau pelanggan toko online Anda. Maka, gunakanlah plugin anti malware di website WordPress Anda.
 
-    ![alt text](images/image17.png)
+    <img src="https://github.com/natsyz/DTS/blob/master/images/image19.png?raw=true" alt="drawing" width="700"/>
 
     Apa saja pilihan plugin yang bisa Anda gunakan? Sebagian besar plugin security WordPress yang pernah kami bahas bisa Anda gunakan. Atau, Anda bisa menggunakan MalCare yang merupakan plugin anti malware dengan fitur instant removal. Malcare bisa mendeteksi adanya malware yang menyerang website dan lalu menghapusnya secara otomatis.
 
@@ -243,7 +243,7 @@ st.markdown("""
 4. **Lakukan Manajemen Pengguna dengan Ketat**
 
     Jika website WordPress Anda dikelola banyak orang, lakukan manajemen pengguna dengan baik. Jangan berikan hak akses admin ke semua orang. Hak akses user harus disesuaikan dengan kewenangan dan kemampuannya dalam menjaga keamanan website dengan baik.
-""")
+""", unsafe_allow_html=True)
 
 
 
